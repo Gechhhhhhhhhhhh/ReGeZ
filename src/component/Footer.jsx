5116cb02288d21c62cd463f87e34fdfe592a0a4f
GECH <sivgach.sim@icloud.com>
2024-12-31 23:26:58 -0800

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -36,8 +36,8 @@ const Footer = () => {
         <div className="">
           <h1 className="font-semibold text-2xl font-primary">Contact</h1>
           <div className="text-base ">
-            <p className="mt-3">Phone: (+855)71 776 0209</p>
-            <p>Email:regenerative.genz@gmail.com</p>
+            <p className="mt-3">Phone: (+855)71-776-0209</p>
+            <p>Email: regenerative.genz@gmail.com</p>
             <p>Address: Online community</p>
           </div>
         </div>
